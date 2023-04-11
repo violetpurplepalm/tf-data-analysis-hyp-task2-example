b19d552f72db2c700cb53cc5211f0944c91cc078
violetpurplepalm <45032239+violetpurplepalm@users.noreply.github.com>
2023-04-11 20:25:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 425154307 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import mannwhitneyu
+    stat, p = mannwhitneyu(x, y)
+    if p > 0.07:
+        return False
+    return True
+
